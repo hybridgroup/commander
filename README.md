466c5bcdce5416551ae0932c5f86ef7c7551e60c
Javier Cervantes <javier@hybridgroup.com>
2014-08-11 14:53:26 -0500

    Initial commit with blank app

---
+++ README.md
@@ -0,0 +1,16 @@
+Commander
+=====================
+
+An app to control robots from your device
+
+## Using this project
+
+TODO
+
+## Installation
+
+```bash
+$ sudo npm install -g cordova ionic gulp
+$ npm install
+$ gulp install
+```
---
+++ www/js/app.js
@@ -0,0 +1,14 @@
+angular.module('commander', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
