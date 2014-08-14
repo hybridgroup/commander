434b6644c452a23f3edbda8972786659a8ff1cf2
Javier Cervantes <javier@hybridgroup.com>
2014-08-14 14:23:33 -0500

    Adding new platforms builds

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -1,4 +1,22 @@
-angular.module('commander', ['ionic'])
+localStorage.commander = JSON.stringify({
+  robot: {
+    host: 'http://localhost:8080',
+    name: 'pebble',
+    device: 'pebble',
+    commands: {
+      up: '',
+      center: 'pending_message',
+      down: '',
+      left: '',
+      right: '',
+      one: '',
+      two: '',
+      three: ''
+    }
+  }
+});
+
+app = angular.module('commander', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -11,4 +29,38 @@ angular.module('commander', ['ionic'])
       StatusBar.styleDefault();
     }
   });
-})
+});
+
+app.controller('CommanderController', ['$http', function($http) {
+  this.message = 'Ready...';
+
+  this.robot = function() {
+    return JSON.parse(localStorage.commander).robot;
+  };
+
+  this.commandUrl = function(buttonName) {
+    var robot = this.robot();
+    return robot.host + '/api/robots/' + robot.name + '/devices/' +
+      robot.device + '/commands/' + robot.commands[buttonName];
+  };
+
+  this.execute = function(buttonName) {
+    command = this.robot().commands[buttonName];
+
+    if ( command === '') {
+      this.message = 'Command is not defined';
+    } else {
+      this.message = 'Executting command: ' + command;
+
+      var execution = $http.post(this.commandUrl(buttonName));
+
+      execution.success(function(data){
+        this.message = 'Result: ' + data.result;
+      }.bind(this));
+
+      execution.catch(function(data){
+        this.message = 'Error executing command: ' + command;
+      }.bind(this));
+    }
+  };
+}]);
