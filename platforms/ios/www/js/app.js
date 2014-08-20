4fdb9880bc381b57be2deef76c2ad505686dd2be
Luis Galaviz <lm.galaviz@live.com.mx>
2014-08-20 13:03:43 -0500

    Adding commander local storage when non existen.

--- platforms/ios/www/js/app.js
+++ platforms/ios/www/js/app.js
@@ -1,18 +1,21 @@
-localStorage.commander = JSON.stringify({
-  api: {
-    host: 'http://localhost',
-    port: '8080'
-  },
-  commands: [{
-    label: 'Say hello',
-    robot: 'pebble',
-    device: 'pebble',
-    name: 'sendNotification',
-    params: {message: 'Hello'}
-  }]
-});
+if(!localStorage.commander){
+  localStorage.commander = JSON.stringify({
+    api: {
+      host: 'http://localhost',
+      port: '8080'
+    },
+    commands: [{
+      label: 'Say hello',
+      robot: 'pebble',
+      device: 'pebble',
+      name: 'sendNotification',
+      params: {message: 'Hello'}
+    }]
+  });
+}
 
-app = angular.module('commander', ['ionic'])
+
+commander = angular.module('commander', ['ionic'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -27,30 +30,3 @@ app = angular.module('commander', ['ionic'])
   });
 });
 
-app.controller('CommanderController', ['$http', function($http) {
-  this.message = 'Ready...';
-
-  this.commands = function() {
-    return JSON.parse(localStorage.commander).commands;
-  };
-
-  this.commandUrl = function(command) {
-    var api = JSON.parse(localStorage.commander).api;
-
-    return api.host + ':' + api.port + '/api/robots/' + command.robot +
-      '/devices/' + command.device + '/commands/' + command.name;
-  };
-
-  this.execute = function(index) {
-    var command = this.commands()[index];
-    var execution = $http.post(this.commandUrl(command), command.params);
-
-    execution.success(function(data){
-      this.message = 'Result of ' + command.name + ': ' + data.result;
-    }.bind(this));
-
-    execution.catch(function(data){
-      this.message = 'Error executing command: ' + command.name;
-    }.bind(this));
-  };
-}]);
