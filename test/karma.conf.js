e337d3d49d2e0a518a7e3e36dc0114586f516bb4
Luis Godinez <luisgodinez@gmail.com>
2014-11-03 11:09:37 -0600

    updating travis configuration

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,5 +1,5 @@
 module.exports = function(config){
-  config.set({
+  var configuration = {
 
     basePath : '../',
 
@@ -16,6 +16,13 @@ module.exports = function(config){
 
     browsers : ['Chrome'],
 
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+      }
+    },
+
     plugins : [
             'karma-chrome-launcher',
             'karma-firefox-launcher',
@@ -27,5 +34,11 @@ module.exports = function(config){
       suite: 'unit'
     }
 
-  });
+  };
+
+  if(process.env.TRAVIS){
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
+
+  config.set(configuration);
 };
