6a9f491208f344d16064e2531f39efec4003de65
Javier Cervantes <1.27201@gmail.com>
2014-11-06 10:31:55 -0600

    Adding dpad cylon example

---
+++ command_sets/sphero/dpad.js
@@ -0,0 +1,44 @@
+var Cylon = require('cylon');
+
+Cylon.config({
+  api: {
+    ssl: false,
+    port: '8080',
+    host: '0.0.0.0',
+  }
+});
+
+Cylon.api();
+
+Cylon.robot({
+  name: 'sphero-dpad',
+  connection: { name: 'sphero', adaptor: 'sphero', port: '/dev/tty.Sphero-YBW-RN-SPP' },
+
+  work: function(my) { },
+  move: function(direction) {
+    switch (direction) {
+      case "up":
+        this.roll(100, 0);
+        break;
+      case "down":
+        this.roll(100, 180);
+        break;
+      case "left":
+        this.roll(100, 270);
+        break;
+      case "right":
+        this.roll(100, 90);
+    }
+
+    setTimeout(function() {
+      this.stop();
+    }, 2000);
+
+    return true;
+  },
+  commands: function() {
+    return {
+      move: this.move
+    };
+  }
+}).start();
