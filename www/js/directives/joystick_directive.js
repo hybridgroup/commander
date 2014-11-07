c5fbc67b2e896ae8e4a2771d690350eb6c43ac59
Luis Godinez <luisgodinez@gmail.com>
2014-11-07 15:39:40 -0600

    adding multiple joysticks and buttons

--- www/js/directives/joystick_directive.js
+++ www/js/directives/joystick_directive.js
@@ -1,14 +1,14 @@
-commander.directive('draggable', function($document) {
+commander.directive('draggable', function($document,$timeout) {
   return function(scope, element, attr) {
-    var startX = 0, startY = 0, x = 0, y = 0, currentXpos = 0, currentYpos = 0, initXpos = 0, initYpos = 0, positionChanged=false;
+    var domJoysticks = [];
+    var joystick_vars = [{startX: 0, startY: 0, x: 0, y: 0, currentXpos: 0, currentYpos: 0, initXpos: 0, initYpos: 0, positionChanged: false},{startX: 0, startY: 0, x: 0, y: 0, currentXpos: 0, currentYpos: 0, initXpos: 0, initYpos: 0, positionChanged: false}];
+    var resetJoysticks = true;
 
-    element.css({
-     position: 'relative',
-     cursor: 'pointer',
-     display: 'block',
-     top: y + 'px',
-     left:  x + 'px'
-    });
+    $timeout(function(){
+      for(var i=0; i<scope.joysticks.length; i++){
+        domJoysticks.push(angular.element(document.querySelector('#joystick'+i)));
+      }
+    }); 
 
     element.on('tap', function(event) {
       event.preventDefault();
@@ -25,81 +25,80 @@ commander.directive('draggable', function($document) {
       return false;
     });
 
-    element.on('dragstart', function(event) {
-      event.preventDefault();
-      event.gesture.preventDefault();
-      startX = event.gesture.center.pageX - x;
-      startY = event.gesture.center.pageY - y;
-      $document.bind('drag', move);
-      $document.bind('dragend', release);
-    });
-
-    function move(event) {
-      x = event.gesture.center.pageX - startX;
-      y = event.gesture.center.pageY - startY;
-
-      if(x < 15 && x > -15) {
-        element.css({
-          left:  x + 'px'
-        });
+    element.on('transform drag', function(event) {
+      joystickIndex = parseInt(event.gesture.target.id.substring(8));
+      var joystick = domJoysticks.filter(function(e){if (e[0].id == 'joystick'+joystickIndex){return e[0];}})[0];
+      var touch;
+      if (event.gesture.touches[0].target.id == 'joystick'+joystickIndex){
+        touch = event.gesture.touches[0]
       }
-      if(y < 15 && y > -15) {
-        element.css({
-          top: y + 'px'
-        });
+      else {
+        touch = event.gesture.touches[1] 
       }
-
-      element.css({
-        'box-shadow': 'inset ' + x + 'px ' + y + 'px 8px rgba(10,10,10,0.3)'
-      });
+      if (resetJoysticks){
+        joystick_vars[joystickIndex].startX = touch.pageX - joystick_vars[joystickIndex].x;
+        joystick_vars[joystickIndex].startY = touch.pageY - joystick_vars[joystickIndex].y;
+      }
+      resetJoysticks = false;
       
-      if (x > 0) { currentXpos = 1;}
-      else if (x < 0) { currentXpos = -1;}
-      else { currentXpos = 0;}
+      joystick_vars[joystickIndex].x = touch.pageX - joystick_vars[joystickIndex].startX;
+      joystick_vars[joystickIndex].y = touch.pageY - joystick_vars[joystickIndex].startY;
 
-      if (y > 0) { currentYpos = -1;}
-      else if (y < 0) { currentYpos = 1;}
-      else { currentYpos = 0;}
+      if(joystick_vars[joystickIndex].x < 20 && joystick_vars[joystickIndex].x > -20) {
+        joystick.css({
+          left:  joystick_vars[joystickIndex].x + 'px'
+        });
+      }
+      if(joystick_vars[joystickIndex].y < 20 && joystick_vars[joystickIndex].y > -20) {
+        joystick.css({
+          top: joystick_vars[joystickIndex].y + 'px'
+        });
+      }
+      
+      if (joystick_vars[joystickIndex].x > 0) { joystick_vars[joystickIndex].currentXpos = 1;}
+      else if (joystick_vars[joystickIndex].x < 0) { joystick_vars[joystickIndex].currentXpos = -1;}
+      else { joystick_vars[joystickIndex].currentXpos = 0;}
 
-      if (initXpos!=currentXpos){
-        initXpos = currentXpos;
-        positionChanged = true;
+      if (joystick_vars[joystickIndex].y > 0) { joystick_vars[joystickIndex].currentYpos = -1;}
+      else if (joystick_vars[joystickIndex].y < 0) { joystick_vars[joystickIndex].currentYpos = 1;}
+      else { joystick_vars[joystickIndex].currentYpos = 0;}
+
+      if (joystick_vars[joystickIndex].initXpos!=joystick_vars[joystickIndex].currentXpos){
+        joystick_vars[joystickIndex].initXpos = joystick_vars[joystickIndex].currentXpos;
+        joystick_vars[joystickIndex].positionChanged = true;
       }
 
-      if (initYpos!=currentYpos){
-        initYpos = currentYpos;
-        positionChanged = true;
+      if (joystick_vars[joystickIndex].initYpos!=joystick_vars[joystickIndex].currentYpos){
+        joystick_vars[joystickIndex].initYpos = joystick_vars[joystickIndex].currentYpos;
+        joystick_vars[joystickIndex].positionChanged = true;
       }
 
-      if (positionChanged) {
-        command = scope.commands[element.attr('data-command-index')];
-        command.params.position = "" + initXpos + "," + initYpos;
+      if (joystick_vars[joystickIndex].positionChanged) {
+        command = scope.commands[joystick.attr('data-command-index')];
+        command.params.position = "" + joystick_vars[joystickIndex].initXpos + "," + joystick_vars[joystickIndex].initYpos;
         scope.execute(command);
-        positionChanged = false;
+        joystick_vars[joystickIndex].positionChanged = false;
       }
       
-    }
-
-    function release() {
-      $document.unbind('drag', move);
-      $document.unbind('dragend', release);
-      x = 0;
-      y = 0;
-      startX = 0;
-      startY = 0;
-      initXpos = 0;
-      initYpos = 0;
-
-      command = scope.commands[element.attr('data-command-index')];
-      command.params.position = "" + initXpos + "," + initYpos;
-      scope.execute(command);
-      positionChanged = false;
-
-      element.css({
-        top: y + 'px',
-        left: x + 'px',
-        'box-shadow': 'inset ' + x + 'px ' + y + 'px 8px rgba(10,10,10,0.3)'
+      joystick.css({
+        'box-shadow': (joystick_vars[joystickIndex].x / 2) + 'px ' + (joystick_vars[joystickIndex].y / 2) + 'px 10px #333'
       });
-    }
+    });
+    
+    element.on('release', function(event) {
+      for(var i=0; i<domJoysticks.length; i++){
+        command = scope.commands[domJoysticks[i].attr('data-command-index')];
+        command.params.position = "0,0";
+        scope.execute(command);
+
+        domJoysticks[i].css({
+          top: '0px',
+          left: '0px',
+          'box-shadow': '0px 0px 20px #333'
+        });
+      }
+      resetJoysticks = true;
+      joystick_vars = [{startX: 0, startY: 0, x: 0, y: 0, currentXpos: 0, currentYpos: 0, initXpos: 0, initYpos: 0, positionChanged: false},{startX: 0, startY: 0, x: 0, y: 0, currentXpos: 0, currentYpos: 0, initXpos: 0, initYpos: 0, positionChanged: false}];
+    });
   };
 });
